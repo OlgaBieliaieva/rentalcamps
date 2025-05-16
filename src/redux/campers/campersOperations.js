7697636114d9c211a34d3cba34674e2db5d5e9dd
Olha Shapoval <112978931+OlgaBieliaieva@users.noreply.github.com>
2025-05-16 17:06:13 +0300

    Update campersOperations.js

--- src/redux/campers/campersOperations.js
+++ src/redux/campers/campersOperations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://6618f1089a41b1b3dfbe61ce.mockapi.io/api/";
+axios.defaults.baseURL = "https://6618f1089a41b1b3dfbe61ce.mockapi.io/api";
 
 export const fetchAll = createAsyncThunk(
   "campers/fetchAll",
